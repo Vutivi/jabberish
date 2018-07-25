06255e69eb88356c6baa0ddd7bfcc8ced1b09c52
vutivi <201141790@student.uj.ac.za>
2018-07-25 12:36:24 +0200

    remove sqllite for heroku in gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to Jabberish!!"
+      flash[:success] = "Welcome to Jabberish new user!!"
       redirect_to @user
     else
       render 'new'
